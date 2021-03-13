803719797bdaaa7f1eb862203392a269d718b91d
xulichen <306084800@qq.com>
2021-03-13 10:12:57 +0800

    feat: grpc add dial with consul && dial directly

--- pkg/net/rpc/client.go
+++ pkg/net/rpc/client.go
@@ -1,12 +1,15 @@
 package rpc
 
 import (
+	"fmt"
 	"github.com/xulichen/halfway/pkg/discovery/consul"
+	"go.elastic.co/apm/module/apmgrpc"
 
 	"google.golang.org/grpc"
 )
 
-func Dial(cr consul.Resource, service string) *grpc.ClientConn {
+// DialWithConsul 客户端通过consul链接服务端
+func DialWithConsul(cr consul.Resource, service string) *grpc.ClientConn {
 	done := cr.ClaimServices([]string{service})
 	if !done {
 		panic("claim service failed")
@@ -16,4 +19,19 @@ func Dial(cr consul.Resource, service string) *grpc.ClientConn {
 		panic(err)
 	}
 	return conn
+}
+
+// Dial 客户端直连服务端... 需配合linkerd或istio做负载均衡
+func Dial(serviceAddr string, options ...grpc.DialOption) (*grpc.ClientConn, error) {
+	options = append(options, grpc.WithInsecure())
+	options = append(options, grpc.WithUnaryInterceptor(apmgrpc.NewUnaryClientInterceptor()))
+
+	conn, err := grpc.Dial(
+		fmt.Sprintf("%s://%s", "tcp", serviceAddr),
+		options...,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
